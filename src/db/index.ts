0dc59f0fdab00c29388c9a3388d469f4486c5a17
Morax25 <adarsh@infoneotech.com>
2026-04-07 17:15:51 +0530

    fixe DB URI

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,18 @@ import mongoose from 'mongoose';
 import { DB_NAME, MONGODB_URI } from '../constant.js';
 
 const connectDB = async () => {
-  const connectionInstance = await mongoose.connect(
-    `${MONGODB_URI}/${DB_NAME}`
-  );
+  console.log(DB_NAME, MONGODB_URI);
+  try {
+    const connection = await mongoose.connect(
+      `${MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`
+    );
 
-  return connectionInstance;
+    console.log('MongoDB connected');
+    return connection;
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    throw err;
+  }
 };
 
 export default connectDB;
