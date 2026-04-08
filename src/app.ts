bad70aaa807bfbda7559810d917971e79cbeefd0
Morax25 <adarsh@infoneotech.com>
2026-04-08 15:34:03 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import mongoose from 'mongoose';
 import morgan from 'morgan';
 import { corsConfig } from './configs/cors.js';
 import { loggerStream } from './configs/logger.js';
 import { healthCheck } from './controller/healthCheck.controller.js';
 import { notFoundHandler } from './controller/notFoundHandler.controller.js';
-import { Property } from './models/property.model.js';
 import propertyRouter from './routes/property.routes.js';
 import userRouter from './routes/user.routes.js';
 import { errorHandler } from './utils/errorHandler.js';
@@ -25,9 +23,6 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 // ✅ compression removed — Vercel CDN handles this at the edge
-
-console.log('mongoose ready:', mongoose.connection.readyState);
-console.log('model ready:', Property.db.readyState);
 
 app.get('/health', healthCheck);
 app.use('/api/v1/user', userRouter);
