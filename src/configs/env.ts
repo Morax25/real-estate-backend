b70ae5019eb22c02dcbc58d46250e74f73d86ae3
Morax25 <adarsh@infoneotech.com>
2026-04-14 11:27:04 +0530

    update

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,9 +1,13 @@
 import { config } from 'dotenv';
+
 const ENV = process.env.NODE_ENV || 'development';
-config();
-config({ path: `.env.${ENV}.local` });
+
+if (ENV !== 'production') {
+  config();
+  config({ path: `.env.${ENV}.local` });
+}
+
 const required = [
-  'PORT',
   'MONGODB_URI',
   'CLOUDINARY_CLOUD_NAME',
   'CLOUDINARY_API_KEY',
@@ -19,8 +23,7 @@ for (const key of required) {
 console.log('NODE_ENV:', JSON.stringify(ENV));
 
 export const NODE_ENV = ENV;
-export const PORT = Number(process.env.PORT);
-export const DB_URI = process.env.DB_URI!;
+export const DB_URI = process.env.MONGODB_URI!;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME!;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY!;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET!;
